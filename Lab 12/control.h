fcc30157bcfa540f9e1ae5830ea358a614a68624
Benjamin Thaut <dasaxmansbrother@hotmail.com>
2020-07-10 06:45:35 -0700

    Lab 12

---
+++ Lab 12/control.h
@@ -0,0 +1,19 @@
+/***********************************************************************
+ * COMPONENT:
+ *    CONTROL
+ * Author:
+ *    Br. Helfrich, <your name here if you made a change>
+ * Summary:
+ *    This class stores the notion of Bell-LaPadula
+ ************************************************************************/
+
+#pragma once
+
+#include <string>
+using namespace std;
+
+enum Control
+{
+	Public, Confidential, Privileged, Secret
+};
+
