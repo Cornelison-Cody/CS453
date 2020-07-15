639bdc710434c029c7f5ae4343c022e3e53aba15
Cornelison-Cody <codysmart@gmail.com>
2020-07-14 22:41:53 -0700

    Psuedo Code Cipher 06

--- cipher06.h
+++ cipher06.h
@@ -12,8 +12,8 @@
 class Cipher06 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
+   virtual std::string getPseudoAuth()  { return "Cody Cornelison"; }
+   virtual std::string getCipherName()  { return "Ragbaby Cipher"; }
    virtual std::string getEncryptAuth() { return "encrypt author"; }
    virtual std::string getDecryptAuth() { return "decrypt author"; }
 
@@ -23,7 +23,7 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+      return std::string("https://www.cryptogram.org/downloads/aca.info/ciphers/Ragbaby.pdf");
    }
    
    /**********************************************************
@@ -36,10 +36,44 @@ public:
 
       // TODO: please format your pseudocode
       // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+      str =  "encrypt(plainText, password)\n";
+      str += "    string alphabet <- generateAlphabet(password)\n";
+      str =  "    int wordCount = 1\n";
+      str =  "    int charCount = 1\n";
+      str += "    FOR c is all chars in plainText\n";
+      str =  "        IF c is a space\n";
+      str =  "            wordCount++\n";
+      str =  "            charCount = wordCount % alphabet.length()\n";
+      str =  "            encryptedText[c] = space\n";
+      str =  "        ELSE \n";
+      str =  "        offsetArray[c] = charCount++ % alphabet.length()\n";
+      str += "        encryptedText[c] = alphabet[(alpabet.find(c) + offsetArray[c]) % alphabet.length()]\n";
+      str += "    RETURN encryptedText\n";
 
       // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+      str =  "decrypt(plainText, password)\n";
+      str += "    string alphabet <- generateAlphabet(password)\n";
+      str =  "    int wordCount = 1\n";
+      str =  "    int charCount = 1\n";
+      str += "    FOR c is all chars in plainText\n";
+      str =  "        IF c is a space\n";
+      str =  "            wordCount++\n";
+      str =  "            charCount = wordCount % alphabet.length()\n";
+      str =  "            decryptedText[c] = space\n";
+      str =  "        ELSE \n";
+      str =  "        offsetArray[c] = charCount++ % alphabet.length()\n";
+      str += "        decryptedText[c] = alphabet[(alphabet.length() + alpabet.find(c) - offsetArray[c]) % alphabet.length()]\n";
+      str += "    RETURN decryptedText\n";
+
+      // Generate Alphabet function
+      str =  "generateAlphabet(password)\n";
+      str += "    FOR p is all values of password\n";
+      str += "        store upper case p in a std::set alphabet to remove duplicates\n";
+      str += "        if p is a - j then add corresponding number to alphabet\n";
+      str += "    FOR c is A - Z and 0 - 9\n";
+      str += "        add to alphabet as well\n";
+      str += "    RETURN alphabet\n";
+      
 
       return str;
    }
