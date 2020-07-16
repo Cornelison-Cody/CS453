7e6d13813ccbb95fddd99f76f040d3f4d08341c8
Eric Mott <mott.eric@sbcglobal.net>
2020-07-15 18:51:29 -0700

    Checkerboard pseudocode
    Added pseudocode

--- cipher02.h
+++ cipher02.h
@@ -1,5 +1,6 @@
 /********************************************************************
 * Header:
+* Checkerboard Cipher
 *    Implement your cipher here. You can view 'example.h' to see the
 *    completed Caesar Cipher example.
 ********************************************************************/
@@ -32,16 +33,59 @@ public:
     **********************************************************/
    virtual std::string getPseudocode()
    {
-      std::string str;
+	   std::string str;
 
-      // TODO: please format your pseudocode
-      // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+	   // TODO: please format your pseudocode
+	   // The encrypt pseudocode
+	   str = "checkerboardCipher()";
+	   str = +"string vertKeyword";
+	   str = +"string horizKeyword";
+	   str = +"char rowKey[5]";
+	   str = +"char columnKey[5]";
+	   str = +"string message";
+	   str = +"string cipherMessage";
 
-      // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+	   str = +"char alphabetArray[5][5] = {";
+	   str = +"{'w', 'c', 'e', 'o', 'v'},";
+	   str = +"{'x', 'b', 'm', 's', 'f'},";
+	   str = +"{'h', 'k', 'q', 'r', 'y'},";
+	   str = +"{'i', 'p', 'z', 'n', 't'},";
+	   str = +"{'u', 'a', 'd', 'g', 'l'}}";
 
-      return str;
+	   str = +"display 'Enter first keyword (5 nonrepeating characters): '";
+	   str = +"accept vertKeyword";
+	   str = +"display 'Enter second keyword (5 nonrepeating characters): '";
+	   str = +"accept horizKeyword";
+	   str = +"display 'Enter message: '";
+
+	   str = +"convert vertKeyword to vertKey";
+	   str = +"convert horizKeyword to columnKey";
+
+	   str = +"loop thru message";
+	   str = +"loop thru alphabetArray";
+	   str = +"match message character to alphabetArray character";
+	   str = +"if message character is 'j' match to alphabetArray character 'i'";
+	   str = +"if not alpha character then skip";
+	   str = +"cipherMessage =+ rowKey[alphabetArray row value]";
+	   str = +"cipherMessage =+ columnKey(alphabetArray column value]";
+
+	   str = +"return codedCheckerboard";
+
+	   // The decrypt pseudocode
+	   str += "decodeCheckerboard(codedCheckerboard)";
+	   str = +"string decodeMessage";
+	   str = +"loop thru codedCheckerboard";
+	   str = +"if codedCheckerboard index is even";
+	   str = +"match codedCheckerboard character to rowKey character";
+	   str = +"alphabetArrayRow = rowKey index";
+	   str = +"if codedCheckerboard index is odd";
+	   str = +"match codedCheckerboard character to columnKey character";
+	   str = +"alphabetArrayColumn = columnKey index";
+	   str = +"decodedMessage =+ alphabetArray[alphabetArrayRow][alphabetArrayColumn]";
+
+	   str = +"return decodedMessage";
+
+	   return str;
    }
 
    /**********************************************************
