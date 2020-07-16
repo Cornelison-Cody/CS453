1c596799ed8d87b9fbd1455b6038f153939cf82d
Jason Steffen <Jason1284@gmail.com>
2020-07-15 18:37:44 -0600

    Fixing notation for compliation.

--- cipher05.h
+++ cipher05.h
@@ -7,6 +7,9 @@
 #ifndef CIPHER05_H
 #define CIPHER05_H
 
+#include <string>
+
+
 /********************************************************************
  * CLASS
  *******************************************************************/
@@ -37,140 +40,139 @@ public:
 
       // TODO: please format your pseudocode
       // The encrypt pseudocode
-      str =  "
-      string plaintext;
-      string password;
-      string cipher;
+      str =  "";
+      str += "string plaintext;\n";
+      str += "string password;\n";
+      str += "string cipher;\n";
       
-      cin >> password;
-      cin >> plaintext;
+      str += "cin >> password;\n";
+      str += "cin >> plaintext;\n";
 
-      char cipherColumn[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+      str += "char cipherColumn[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}\n";
 
-      char aArray[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
-      char bArray[] = {b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a}
-      char cArray[] = {c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b}
-      ...
-      //THE REST OF THE ARRAYS GO HERE
-      ...
-      char zArray[] = {z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y}
+      str += "char aArray[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}\n";
+      str += "char bArray[] = {b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a}\n";
+      str += "char cArray[] = {c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b}\n";
+      str += "...\n";
+      str += "//THE REST OF THE ARRAYS GO HERE\n";
+      str += "...\n";
+      str += "char zArray[] = {z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y}\n";
 
-      int passwordIterator = 0;
+      str += "int passwordIterator = 0;\n";
 
-      //Iterate through the plaintext message
-      for int i = 0; plaintext[i]; i++
-      {
-         char plaintextLetter = plaintext[i]
-         int plaintextNumber = -1;
-         char passwordLetter = password[passwordIterator]
+      str += "//Iterate through the plaintext message\n";
+      str += "for int i = 0; plaintext[i]; i++\n";
+      str += "{\n";
+      str += "   char plaintextLetter = plaintext[i]\n";
+      str += "   int plaintextNumber = -1;\n";
+      str += "   char passwordLetter = password[passwordIterator]\n";
 
-         //Find the column number of the cipher
-         SWITCH statement
-         char plaintextLetter = a
-            plaintextNumber = 0;
-            break;
-         char plaintextLetter = b
-            plaintextNumber = 1;
-            break;
-         //...MORE SWITCH STATEMENTS
-         char plaintextLetter = z
-            plaintextNumber = 25
-            break;
+      str += "   //Find the column number of the cipher\n";
+      str += "   SWITCH statement\n";
+      str += "   char plaintextLetter = a\n";
+      str += "      plaintextNumber = 0;\n";
+      str += "      break;\n";
+      str += "   char plaintextLetter = b\n";
+      str += "      plaintextNumber = 1;\n";
+      str += "      break;\n";
+      str += "   //...MORE SWITCH STATEMENTS\n";
+      str += "   char plaintextLetter = z\n";
+      str += "      plaintextNumber = 25\n";
+      str += "      break;\n";
 
-         //Any character that is not a-z will be the placeholder
-         if plaintextNumber = -1
-            cipher[i] = 0 or any other placeholder
+      str += "   //Any character that is not a-z will be the placeholder\n";
+      str += "   if plaintextNumber = -1\n";
+      str += "      cipher[i] = 0 or any other placeholder\n";
 
-         //Find the row of the cipher and set the cipher's letter
-         SWITCH statement
-         char password[passwordIterator] = a
-            cipher[i] = aArray[plaintextNumber];
-            break;
-         char password[passwordIterator] = b
-            cipher[i] = bArray[plaintextNumber];
-            break;
-         //...MORE SWITCH STATEMENTS
-         char password[passwordIterator] = z
-            cipher[i] = zArray[plaintextNumber];
-            break;
+      str += "   //Find the row of the cipher and set the cipher's letter\n";
+      str += "   SWITCH statement\n";
+      str += "   char password[passwordIterator] = a\n";
+      str += "      cipher[i] = aArray[plaintextNumber];\n";
+      str += "      break;\n";
+      str += "   char password[passwordIterator] = b\n";
+      str += "      cipher[i] = bArray[plaintextNumber];\n";
+      str += "      break;\n";
+      str += "   //...MORE SWITCH STATEMENTS\n";
+      str += "   char password[passwordIterator] = z\n";
+      str += "      cipher[i] = zArray[plaintextNumber];\n";
+      str += "      break;\n";
 
-            //If we have gone through the password, start over.
-         if password[!passwordIterator++]
-           passwordIterator = 0;
-         else passwordIterator++;
-      }
+      str += "      //If we have gone through the password, start over.\n";
+      str += "   if password[!passwordIterator++]\n";
+      str += "     passwordIterator = 0;\n";
+      str += "   else passwordIterator++;\n";
+      str += "}\n";
 
-         cout << cipher;
-      ";
+      str += "   cout << cipher;\n";
+      
+      
 
       // The decrypt pseudocode
-      str += "
-      string plaintext;
-      string password;
-      string cipher;
+      str += "\n";
+      str += "string plaintext;\n";
+      str += "string password;\n";
+      str += "string cipher;\n";
       
-      cin >> password;
-      cin >> cipher;
+      str += "cin >> password;\n";
+      str += "cin >> cipher;\n";
 
-      char plaintextColumn[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+      str += "char plaintextColumn[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}\n";
 
-      char aArray[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
-      char bArray[] = {b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a}
-      char cArray[] = {c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b}
-      ...
-      //THE REST OF THE ARRAYS GO HERE
-      ...
-      char zArray[] = {z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y}
+      str += "char aArray[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}\n";
+      str += "char bArray[] = {b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a}\n";
+      str += "char cArray[] = {c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,a,b}\n";
+      str += "...\n";
+      str += "//THE REST OF THE ARRAYS GO HERE\n";
+      str += "...\n";
+      str += "char zArray[] = {z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y}\n";
 
-      int passwordIterator = 0;
+      str += "int passwordIterator = 0;\n";
 
-      //Iterate through the cipher message
-      for int i = 0; cipher[i]; i++
-      {
-         char cipherLetter = cipher[i]
-         int cipherNumber = -1;
-         char passwordLetter = password[passwordIterator]
+      str += "//Iterate through the cipher message\n";
+      str += "for int i = 0; cipher[i]; i++\n";
+      str += "{\n";
+      str += "   char cipherLetter = cipher[i]\n";
+      str += "   int cipherNumber = -1;\n";
+      str += "   char passwordLetter = password[passwordIterator]\n";
 
-         //Find the column number of the cipher
-         SWITCH statement
-         char cipherLetter = a
-            cipherNumber = 0;
-            break;
-         char cipherLetter = b
-            cipherNumber = 1;
-            break;
-         //...MORE SWITCH STATEMENTS
-         char cipherLetter = z
-            cipherNumber = 25
-            break;
+      str += "   //Find the column number of the cipher\n";
+      str += "   SWITCH statement\n";
+      str += "   char cipherLetter = a\n";
+      str += "      cipherNumber = 0;\n";
+      str += "      break;\n";
+      str += "   char cipherLetter = b\n";
+      str += "      cipherNumber = 1;\n";
+      str += "      break;\n";
+      str += "   //...MORE SWITCH STATEMENTS\n";
+      str += "   char cipherLetter = z\n";
+      str += "      cipherNumber = 25\n";
+      str += "      break;\n";
 
-         //Any character that is not a-z will be the placeholder
-         if cipherNumber = -1
-            plaintext[i] = 0 or any other placeholder
+      str += "   //Any character that is not a-z will be the placeholder\n";
+      str += "   if cipherNumber = -1\n";
+      str += "      plaintext[i] = 0 or any other placeholder\n";
 
-         //Find the row of the plaintext and set the plaintext's letter
-         SWITCH statement
-         char password[passwordIterator] = a
-            plaintext[i] = aArray[cipherNumber];
-            break;
-         char password[passwordIterator] = b
-            plaintext[i] = bArray[cipherNumber];
-            break;
-         //...MORE SWITCH STATEMENTS
-         char password[passwordIterator] = z
-            plaintext[i] = zArray[cipherNumber];
-            break;
+      str += "   //Find the row of the plaintext and set the plaintext's letter\n";
+      str += "   SWITCH statement\n";
+      str += "   char password[passwordIterator] = a\n";
+      str += "      plaintext[i] = aArray[cipherNumber];\n";
+      str += "      break;\n";
+      str += "   char password[passwordIterator] = b\n";
+      str += "      plaintext[i] = bArray[cipherNumber];\n";
+      str += "      break;\n";
+      str += "   //...MORE SWITCH STATEMENTS\n";
+      str += "   char password[passwordIterator] = z\n";
+      str += "      plaintext[i] = zArray[cipherNumber];\n";
+      str += "      break;\n";
 
-         //If we have gone through the password, start over.
-         if password[!passwordIterator++]
-           passwordIterator = 0;
-         else passwordIterator++;
+      str += "   //If we have gone through the password, start over.\n";
+      str += "   if password[!passwordIterator++]\n";
+      str += "     passwordIterator = 0;\n";
+      str += "   else passwordIterator++;\n";
 
-      }
+      str += "}\n";
 
-         cout << plaintext;
-
-      ";
+      str += "   cout << plaintext;\n";
 
       return str;
    }
